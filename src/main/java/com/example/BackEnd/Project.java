e46907636caa5a278b3110b00b614287f28b7282
liamholland <liamholland02@gmail.com>
2023-06-09 14:18:02 +0100

    redid Project class

--- src/main/java/com/example/BackEnd/Project.java
+++ src/main/java/com/example/BackEnd/Project.java
@@ -1,41 +1,38 @@
 package com.example.BackEnd;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Hashtable;
 
 //collections of tasks related to a single project
 //should be able to operate like sprints and allow you to plan them
 //access from the day viewer in order to add the various tasks to the correct days
 public class Project implements Serializable{
-    private Hashtable<Task, Integer> tasksWithDurations;    //list of tasks and their associated estimated duration in days
-    private double completionPercentage;
+    private Hashtable<String, LocalDate> tasks;    //list of tasks and their associated date they are scheduled for
+    // private double completionPercentage;
 
     public Project(){
-        completionPercentage = 0.0;
+        // completionPercentage = 0.0;
     }
 
     //add a task to a project
-    public void addTaskToProject(Task task, int durationInDays){
-        if(!tasksWithDurations.keySet().contains(task)){
-            tasksWithDurations.put(task, durationInDays);
-            task.setProject(this);
-            recalculatePrecentageCompletion();
+    public void addTaskToProject(Task task){
+        //check if the task is already in the project
+        if(!(tasks.keySet().contains(task.getName()))){
+            task.setProject(this);  //assign this task to the project
+            tasks.put(task.getName(), task.getDay().getDate());
         }
     }
 
-    public Task[] getTasks(){
-        return tasksWithDurations.keySet().toArray(new Task[0]);
+    public String[] getTasks(){
+        return tasks.keySet().toArray(new String[0]);
     }
 
-    public double getCompletion(){
-        return completionPercentage;
+    public LocalDate[] getDates(){
+        return tasks.entrySet().toArray(new LocalDate[0]);
     }
 
-    private void recalculatePrecentageCompletion(){
-        int numCompleted = 0;
-        for(Task t : tasksWithDurations.keySet()){
-            if(t.isCompleted()) numCompleted++;
-        }
-        completionPercentage = (numCompleted / tasksWithDurations.size()) * 100;
-    }
+    // public double getCompletion(){
+    //     return completionPercentage;
+    // }
 }
